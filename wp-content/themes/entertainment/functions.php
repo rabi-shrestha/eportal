08111e54bde3cf1503ab70251289350b5e802d18
Rabi Shrestha <rshrestha@qkly.io>
2025-01-11 09:44:39 +0545

    Header menu updated

--- wp-content/themes/entertainment/functions.php
+++ wp-content/themes/entertainment/functions.php
@@ -140,3 +140,43 @@ function get_search_results($search_term) {
 
 // add_action('wp_ajax_load_page', 'handle_ajax_navigation');   // For logged-in users
 // add_action('wp_ajax_nopriv_load_page', 'handle_ajax_navigation');  // For non-logged-in users
+
+function register_my_menu() {
+    register_nav_menus( array(
+        'main_menu' => __( 'Main Menu', 'entertainment' ),
+    ) );
+}
+add_action( 'after_setup_theme', 'register_my_menu' );
+
+class Entertainment_Walker_Nav_Menu extends Walker_Nav_Menu {
+    // Starts the list before the elements are added.
+    function start_lvl( &$output, $depth = 0, $args = null ) {
+        $indent = str_repeat("\t", $depth);
+        $output .= "\n$indent<ul class=\"dropdown-menu header__dropdown-menu\">\n";
+    }
+
+    // Starts the element output.
+    function start_el( &$output, $item, $depth = 0, $args = null, $id = 0 ) {
+        $classes = empty( $item->classes ) ? array() : (array) $item->classes;
+        $classes[] = 'header__nav-item';
+
+        $class_names = join(' ', apply_filters('nav_menu_css_class', array_filter($classes), $item, $args));
+        $class_names = $class_names ? ' class="' . esc_attr($class_names) . '"' : '';
+
+        $output .= '<li' . $class_names . '>';
+
+        $attributes  = ! empty( $item->attr_title ) ? ' title="' . esc_attr( $item->attr_title ) . '"' : '';
+        $attributes .= ! empty( $item->target ) ? ' target="' . esc_attr( $item->target ) . '"' : '';
+        $attributes .= ! empty( $item->xfn ) ? ' rel="' . esc_attr( $item->xfn ) . '"' : '';
+        $attributes .= ! empty( $item->url ) ? ' href="' . esc_attr( $item->url ) . '"' : '';
+        $attributes .= ' class="header__nav-link"';
+
+        $item_output = $args->before;
+        $item_output .= '<a' . $attributes . '>';
+        $item_output .= $args->link_before . apply_filters('the_title', $item->title, $item->ID) . $args->link_after;
+        $item_output .= '</a>';
+        $item_output .= $args->after;
+
+        $output .= apply_filters('walker_nav_menu_start_el', $item_output, $item, $depth, $args);
+    }
+}
--- wp-content/themes/entertainment/header.php
+++ wp-content/themes/entertainment/header.php
@@ -19,75 +19,14 @@
 						</a>
 						<!-- end header logo -->
 
-                        <nav class="main-menu">
-        <?php
-        wp_nav_menu( array(
-            'theme_location' => 'main_menu', 
-            'container' => false, // No container, just the menu itself
-            'menu_class' => 'menu-items', // Class for the <ul> element
-        ) );
-        ?>
-    </nav>
-    
-						<!-- header nav -->
-						<ul class="header__nav">
-							<!-- dropdown -->
-							<li class="header__nav-item">
-								<a href="<?php echo esc_url(home_url('/')); ?>" class="header__nav-link">Home</a>
-							</li>
-							<!-- end dropdown -->
-
-							<li class="header__nav-item">
-								<a href="<?php echo esc_url(home_url('/shows')); ?>" class="header__nav-link">Shows</a>
-							</li>
-
-							<!-- dropdown -->
-							<li class="header__nav-item">
-								<a class="header__nav-link" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Catalog <i class="ti ti-chevron-down"></i></a>
-
-								<ul class="dropdown-menu header__dropdown-menu">
-									<li><a href="catalog.html">Catalog style 1</a></li>
-									<li><a href="catalog2.html">Catalog style 2</a></li>
-									<li><a href="details.html">Details Movie</a></li>
-									<li><a href="details2.html">Details TV Series</a></li>
-								</ul>
-							</li>
-							<!-- end dropdown -->
-
-							<li class="header__nav-item">
-								<a href="pricing.html" class="header__nav-link">Pricing plan</a>
-							</li>
-
-							<!-- dropdown -->
-							<li class="header__nav-item">
-								<a class="header__nav-link" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Pages <i class="ti ti-chevron-down"></i></a>
-
-								<ul class="dropdown-menu header__dropdown-menu">
-									<li><a href="about.html">About Us</a></li>
-									<li><a href="profile.html">Profile</a></li>
-									<li><a href="actor.html">Actor</a></li>
-									<li><a href="contacts.html">Contacts</a></li>
-									<li><a href="faq.html">Help center</a></li>
-									<li><a href="privacy.html">Privacy policy</a></li>
-									<li><a href="https://hotflix.volkovdesign.com/admin/index.html" target="_blank">Admin pages</a></li>
-								</ul>
-							</li>
-							<!-- end dropdown -->
-
-							<!-- dropdown -->
-							<li class="header__nav-item">
-								<a class="header__nav-link header__nav-link--more" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false"><i class="ti ti-dots"></i></a>
-
-								<ul class="dropdown-menu header__dropdown-menu">
-									<li><a href="signin.html">Sign in</a></li>
-									<li><a href="signup.html">Sign up</a></li>
-									<li><a href="forgot.html">Forgot password</a></li>
-									<li><a href="404.html">404 Page</a></li>
-								</ul>
-							</li>
-							<!-- end dropdown -->
-						</ul>
-						<!-- end header nav -->
+                        <?php
+							wp_nav_menu( array(
+								'theme_location' => 'main_menu', // The menu location registered in functions.php
+								'container' => false, // Remove any container wrapping
+								'menu_class' => 'header__nav', // Class for the <ul>
+								'walker' => new Entertainment_Walker_Nav_Menu(), // Use the custom walker
+							) );
+						?>
 
 						<!-- header auth -->
 						<div class="header__auth">
