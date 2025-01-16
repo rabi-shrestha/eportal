b4bfcc754676c588964b6121c0dfe531ac04d370
Rabi Shrestha <rshrestha@qkly.io>
2025-01-16 20:14:16 +0545

    404 page updated and eportal database updated

--- wp-content/themes/entertainment/404.php
+++ wp-content/themes/entertainment/404.php
@@ -1,10 +1,21 @@
 <?php
 get_header();
 ?>
-<article>
-    <h2>Page Not Found</h2>
-    <p>Sorry, but the page you are looking for doesn't exist.</p>
-</article>
+<div class="page-404 section--bg" data-bg="<?php echo get_template_directory_uri(); ?>/img/bg/section__bg.jpg" style="background: url(<?php echo get_template_directory_uri(); ?>/img/bg/section__bg.jpg) center center / cover no-repeat;">
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <div class="page-404__wrap">
+                    <div class="page-404__content">
+                        <h1 class="page-404__title">404</h1>
+                        <p class="page-404__text">The page you are looking for is <br>not available!</p>
+                        <a href="<?php echo esc_url(home_url('/')); ?>" class="page-404__btn">go back</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 <?php
 get_footer();
 ?>
